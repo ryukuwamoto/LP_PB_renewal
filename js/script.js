ff4a397d94859b4a956a3b16b32cda6f8e217a6a
ryukuwamoto <ipoi1026oda@gmail.com>
2026-07-20 17:20:37 +0900

    2026.7.20-5.5h

--- js/script.js
+++ js/script.js
@@ -318,3 +318,87 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 });
 
+// =========================================================
+// application プルダウン
+// =========================================================
+document.addEventListener('DOMContentLoaded', () => {
+  const tabs = document.querySelectorAll('.a-top_b__tab');
+  const panels = document.querySelectorAll('.a-top_b__panel');
+
+  tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+      const targetTab = tab.getAttribute('data-tab');
+      tabs.forEach(t => {
+        t.classList.remove('is-active');
+        t.setAttribute('aria-selected', 'false');
+      });
+      tab.classList.add('is-active');
+      tab.setAttribute('aria-selected', 'true');
+      panels.forEach(panel => {
+        panel.setAttribute('hidden', '');
+      });
+      const targetPanel = document.querySelector(`.a-top_b__panel[data-panel="${targetTab}"]`);
+      if (targetPanel) {
+        targetPanel.removeAttribute('hidden');
+      }
+    });
+  });
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  const selectWrapper = document.querySelector('.a-top_b__selects');
+  if (!selectWrapper) return;
+
+  const selects = selectWrapper.querySelectorAll('select');
+  const simTypeSelect = selects[0];
+  const deliverySelect = selects[2];
+  const optionsMap = {
+    default: `<option value="" selected disabled hidden>SIMタイプから選択してください</option>`,
+
+    esim: `
+      <option value="" selected disabled hidden>必要事項の確認</option>
+      <option value="0" data-plm="1">eSIM搭載端末か確認しメールでの配送を了承した</option>
+    `,
+
+    simCard: `
+      <option value="" selected disabled hidden>配送方法を選択</option>
+      <option value="0" data-plm="1">日本国内ポスト投函(＋0円)</option>
+      <option value="550" data-plm="S550">日時指定(＋550円)</option>
+      <option value="1100" data-plm="S1100">日時指定/北海道・沖縄・離島(＋1,100円)</option>
+      <option value="1100" data-plm="K1100">オーストラリア配送(＋1,100円)</option>
+    `
+  };
+
+  function updateDeliveryOptions() {
+    const selectedValue = simTypeSelect.value;
+
+    if (selectedValue === "3500") {
+      deliverySelect.innerHTML = optionsMap.esim;
+    } else if (selectedValue === "3000") {
+      deliverySelect.innerHTML = optionsMap.simCard;
+    } else {
+      deliverySelect.innerHTML = optionsMap.default;
+    }
+  }
+
+  simTypeSelect.addEventListener('change', updateDeliveryOptions);
+
+  updateDeliveryOptions();
+});
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const select = document.querySelector('.js-link-select');
+
+  if (select) {
+    select.addEventListener('change', (e) => {
+      const url = e.target.value;
+      
+      // 選択された値が空や「#」でない場合にページ遷移する
+      if (url && url !== '#') {
+        window.location.href = url;
+      }
+    });
+  }
+});
